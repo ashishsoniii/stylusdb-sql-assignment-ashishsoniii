e5a62f4547a559487bf477b4a70cef73d2520d6f
Ashish Soni <ashishsoni2002@gmail.com>
2024-04-07 23:32:33 +0530

    Step9: Implementing LEFT and RIGHT JOINs

--- tests/step-05/index.test.js
+++ tests/step-05/index.test.js
@@ -1,11 +1,11 @@
 const readCSV = require("../../src/csvReader");
-const parseQuery = require("../../src/queryParser");
+const {parseQuery} = require("../../src/queryParser");
 const executeSELECTQuery = require("../../src/index");
 
 test("Read CSV File", async () => {
   const data = await readCSV("./student.csv");
   expect(data.length).toBeGreaterThan(0);
-  expect(data.length).toBe(3);
+  expect(data.length).toBe(4);
   expect(data[0].name).toBe("John");
   expect(data[0].age).toBe("30"); //ignore the string type here, we will fix this later
 });
@@ -19,6 +19,7 @@ test("Parse SQL Query", () => {
     whereClauses: [],
     joinCondition: null,
     joinTable: null,
+    joinType:null
   });
 });
 
@@ -47,6 +48,7 @@ test("Parse SQL Query with WHERE Clause", () => {
     ],
     "joinCondition": null,
     joinTable: null,
+    joinType: null
   });
 });
 
