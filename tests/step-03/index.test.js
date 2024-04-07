51186b01471661630b537e1aed7bdee6c968b054
Ashish Soni <ashishsoni2002@gmail.com>
2024-04-07 20:33:45 +0530

    Step 8: Add support for Inner Join & Update CSV sample to student

--- tests/step-03/index.test.js
+++ tests/step-03/index.test.js
@@ -2,7 +2,7 @@ const readCSV = require("../../src/csvReader");
 const parseQuery = require("../../src/queryParser");
 
 test("Read CSV File", async () => {
-  const data = await readCSV("./sample.csv");
+  const data = await readCSV("./student.csv");
   expect(data.length).toBeGreaterThan(0);
   expect(data.length).toBe(3);
   expect(data[0].name).toBe("John");
@@ -10,21 +10,25 @@ test("Read CSV File", async () => {
 });
 
 test("Parse SQL Query", () => {
-  const query = "SELECT id, name FROM sample";
+  const query = "SELECT id, name FROM student";
   const parsed = parseQuery(query);
   expect(parsed).toEqual({
     fields: ["id", "name"],
-    table: "sample",
+    table: "student",
     whereClauses: [],
+    "joinCondition": null,
+    joinTable: null
   });
 });
 
 test('Parse SQL Query with WHERE clause', () => {
-    const query = 'SELECT id, name FROM sample WHERE age = 30';
+    const query = 'SELECT id, name FROM student WHERE age = 30';
     const parsed = parseQuery(query);
     expect(parsed).toEqual({
         fields: ['id', 'name'],
-        table: 'sample',
+        table: 'student',
+        joinCondition: null,
+        joinTable: null,
         whereClauses: [{"field" : "age"
         ,"operator" : "="
         ,"value" : "30"}]
@@ -32,11 +36,11 @@ test('Parse SQL Query with WHERE clause', () => {
 });
 
 test('Parse Invalid SQL Query -No FROM - Throw Error', () => {
-    const invalidQuery = 'SELECT id name sample';
-    expect(() => parseQuery(invalidQuery)).toThrow('Invalid query format');
+    const invalidQuery = 'SELECT id name student';
+    expect(() => parseQuery(invalidQuery)).toThrow('Invalid SELECT format');
 });
 
 test('Parse Invalid SQL Query  - Throw Error', () => {
-    const invalidQuery = 'id name FROM sample';
-    expect(() => parseQuery(invalidQuery)).toThrow('Invalid query format');
+    const invalidQuery = 'id name FROM student';
+    expect(() => parseQuery(invalidQuery)).toThrow('Invalid SELECT format');
 });
