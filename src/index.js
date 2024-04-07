20005dd5037a503f4fd9d3fc918d2750c7d9995f
Ashish Soni <ashishsoni2002@gmail.com>
2024-04-07 17:52:07 +0530

    Step4: Integrate CSV with QP

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 // main file
+
+const parseQuery = require('./queryParser');
+const readCSV = require('./csvReader');
+
+async function executeSELECTQuery(query) {
+    const { fields, table } = parseQuery(query);
+    const data = await readCSV(`${table}.csv`);
+    
+    // Filters the fields based on the query
+    return data.map(row => {
+        const filteredRow = {};
+        fields.forEach(field => {
+            filteredRow[field] = row[field];
+        });
+        return filteredRow;
+    });
+}
+
+module.exports = executeSELECTQuery;
