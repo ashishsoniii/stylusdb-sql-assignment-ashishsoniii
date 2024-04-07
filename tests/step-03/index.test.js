a86edd4e2a557d9b1b1f524710e5ebb212efbb45
Ashish Soni <ashishsoni2002@gmail.com>
2024-04-07 17:25:16 +0530

    Step3 - Extended - Adding more tests

--- tests/step-03/index.test.js
+++ tests/step-03/index.test.js
@@ -1,19 +1,29 @@
-const readCSV = require('../../src/csvReader');
-const parseQuery = require('../../src/queryParser');
+const readCSV = require("../../src/csvReader");
+const parseQuery = require("../../src/queryParser");
 
-test('Read CSV File', async () => {
-    const data = await readCSV('./sample.csv');
-    expect(data.length).toBeGreaterThan(0);
-    expect(data.length).toBe(3);
-    expect(data[0].name).toBe('John');
-    expect(data[0].age).toBe('30'); //ignore the string type here, we will fix this later
+test("Read CSV File", async () => {
+  const data = await readCSV("./sample.csv");
+  expect(data.length).toBeGreaterThan(0);
+  expect(data.length).toBe(3);
+  expect(data[0].name).toBe("John");
+  expect(data[0].age).toBe("30"); //ignore the string type here, we will fix this later
 });
 
-test('Parse SQL Query', () => {
-    const query = 'SELECT id, name FROM sample';
-    const parsed = parseQuery(query);
-    expect(parsed).toEqual({
-        fields: ['id', 'name'],
-        table: 'sample'
-    });
+test("Parse SQL Query", () => {
+  const query = "SELECT id, name FROM sample";
+  const parsed = parseQuery(query);
+  expect(parsed).toEqual({
+    fields: ["id", "name"],
+    table: "sample",
+  });
+});
+
+test('Parse Invalid SQL Query -No FROM - Throw Error', () => {
+    const invalidQuery = 'SELECT id name sample';
+    expect(() => parseQuery(invalidQuery)).toThrow('Invalid query format');
+});
+
+test('Parse Invalid SQL Query  - Throw Error', () => {
+    const invalidQuery = 'id name FROM sample';
+    expect(() => parseQuery(invalidQuery)).toThrow('Invalid query format');
 });
